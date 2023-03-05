10b5232a6c0189e384e54c3e4e18ddc79e9e7ed8
Asif Masood Butt <asifmasoodbutt@Asifs-MacBook-Pro.local>
2023-03-06 02:55:45 +0500

    migrations developed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->date('date_of_birth');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('current_address');
+            $table->text('permanent_address');
+            $table->date('joining_date');
+            $table->date('quit_date');
+            $table->date('phone_no');
+            $table->date('father_name');
+            $table->date('salary');
+            $table->date('bank_name');
+            $table->date('bank_account_no');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
